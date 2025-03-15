cf28673d091a724155555c641c84d5f482a2e793
Sebastian <sebastianabraham2006@gmail.com>
2025-03-15 17:59:00 +0530

    feat: added fucntion to verify user based on jwt tokens

--- goapi/controller/UserFunctions.go
+++ goapi/controller/UserFunctions.go
@@ -5,9 +5,11 @@ import (
 	"os"
 	"time"
 
+	"github.com/gofiber/fiber/v2"
 	"github.com/golang-jwt/jwt/v4"
 	"github.com/joho/godotenv"
 	"github.com/streamium-labs/goapi/models"
+	"gorm.io/gorm"
 )
 
 func GenerateJWT(user *models.User) (string, error) {
@@ -33,4 +35,46 @@ func GenerateJWT(user *models.User) (string, error) {
 		return "", err
 	}
 	return tokenString, nil
+}
+
+func VerifyUser(db *gorm.DB) func(*fiber.Ctx) error {
+	return func(c *fiber.Ctx) error {
+		tokenString := c.Get("Authorization")
+		if tokenString == "" {
+			return c.Status(401).JSON(fiber.Map{
+				"message": "No token found",
+			})
+		}
+
+		tokenString = tokenString[7:]
+
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, errors.New("invalid token Here")
+			}
+			return []byte(os.Getenv("SECRET_KEY")), nil
+		})
+
+		if err != nil || !token.Valid {
+			return c.Status(402).JSON(fiber.Map{
+				"message": "Invalid token",
+				"error":   err.Error(),
+			})
+		}
+
+		claims, _ := token.Claims.(jwt.MapClaims)
+		UserID := claims["UserID"]
+		if UserID == nil {
+			return c.Status(403).JSON(fiber.Map{
+				"message": "User Id not found in tokens",
+				"calims":  claims,
+			})
+		}
+
+		return c.Status(200).JSON(fiber.Map{
+			"message": "User verified",
+			"id":      UserID,
+		})
+
+	}
 }
