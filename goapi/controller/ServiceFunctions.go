7bb44ec1c6baa0e3f16fcfe81bfd49704341a7d2
Sebastian <sebastianabraham2006@gmail.com>
2025-03-15 19:26:18 +0530

    feat: added function for getting serevice by id

--- goapi/controller/ServiceFunctions.go
+++ goapi/controller/ServiceFunctions.go
@@ -1,6 +1,8 @@
 package controller
 
 import (
+	"strings"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/streamium-labs/goapi/models"
 	"gorm.io/gorm"
@@ -29,6 +31,32 @@ func CreateService(db *gorm.DB) func(*fiber.Ctx) error {
 
 		return c.Status(201).JSON(fiber.Map{
 			"message": "Blog created",
+			"data":    service,
+		})
+	}
+}
+
+func GetService(db *gorm.DB) func(*fiber.Ctx) error {
+	return func(c *fiber.Ctx) error {
+		id := c.Params("id")
+
+		service := new(models.Service)
+		err := db.Where("service_id = ?", id).First(service).Error
+		if err != nil {
+			if strings.Contains(err.Error(), "record not found") {
+				return c.Status(404).JSON(fiber.Map{
+					"message": "Service not found",
+				})
+			}
+
+			return c.Status(500).JSON(fiber.Map{
+				"message": "Could not retrieve service",
+				"error":   err.Error(),
+			})
+		}
+
+		return c.Status(200).JSON(fiber.Map{
+			"message": "Retrieved service",
 			"data":    service,
 		})
 	}
