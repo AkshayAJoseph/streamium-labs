f2c8bca51671e522681c6bfa438979b23e5f7a03
Sebastian <sebastianabraham2006@gmail.com>
2025-03-16 02:53:15 +0530

    feat: integrated login

---
+++ src/script.js
@@ -0,0 +1,29 @@
+const baseUrl = "https://api.laddu.cc/api/v1";
+
+async function setToken(token) {
+    localStorage.setItem("token", token);
+  }
+
+  async function login(data) {
+    try {
+      console.log(data);
+      const response = await fetch(`${baseUrl}/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      const res = await response.json();
+      if (!response.ok) {
+        alert(res.message);
+        return;
+      }
+      await setToken(res.token);
+      goto("/features", { replaceState: true });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  export { login };
