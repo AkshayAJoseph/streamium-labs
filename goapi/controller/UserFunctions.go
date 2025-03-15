4d0792534cd549845e9fe2a9bb48bd4a5d6e47ff
Sebastian <sebastianabraham2006@gmail.com>
2025-03-15 18:02:12 +0530

    feat: added function to get user by id

--- goapi/controller/UserFunctions.go
+++ goapi/controller/UserFunctions.go
@@ -236,3 +236,29 @@ func UpdateUser(db *gorm.DB) func(*fiber.Ctx) error {
 		})
 	}
 }
+
+func GetUser(db *gorm.DB) func(*fiber.Ctx) error {
+	return func(c *fiber.Ctx) error {
+		id := c.Params("id")
+
+		user := new(models.User)
+		err := db.Where("user_id = ?", id).First(user).Error
+		if err != nil {
+			if strings.Contains(err.Error(), "record not found") {
+				return c.Status(404).JSON(fiber.Map{
+					"message": "User not found",
+				})
+			}
+
+			return c.Status(500).JSON(fiber.Map{
+				"message": "Could not retrieve user",
+				"error":   err.Error(),
+			})
+		}
+
+		return c.Status(200).JSON(fiber.Map{
+			"message": "Retrieved user",
+			"data":    user,
+		})
+	}
+}
