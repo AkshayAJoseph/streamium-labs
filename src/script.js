a0823970b15680d14845035c3b2ac11f250ba6ea
Sebastian <sebastianabraham2006@gmail.com>
2025-03-16 07:31:30 +0530

    feat: added blogging

--- src/script.js
+++ src/script.js
@@ -58,7 +58,7 @@ async function setToken(token) {
     const { value } = await Storage.get({ key: "token" });
     console.log(value);
     if (!value) {
-      goto("login", { replaceState: true });
+      goto("/login", { replaceState: true });
       return;
     }
     const response = await fetch(`${baseUrl}/verify`, {
@@ -107,4 +107,38 @@ async function setToken(token) {
             return res.data;
     }
 
-  export { login , signup, checkUser, getService};
+
+    async function getBlog() {
+        const response = await fetch(`${baseUrl}/blog`, {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          });
+          const res = await response.json();
+          if (!response.ok) {
+            alert(res.message);
+            return;
+          }
+            console.log('done')
+            return res.data;
+    }
+
+
+async function addBlog(data){
+    const response = await fetch(`${baseUrl}/blog`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      const res = await response.json();
+          if (!response.ok) {
+            alert(res.message);
+            return;
+          }
+            console.log(res.data)
+}
+
+  export { login , signup, checkUser, getService, getBlog, addBlog};
